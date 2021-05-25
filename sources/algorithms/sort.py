89bf7a05bd3c3db6155331b118747beae98e1046
Zsófia Limbek <zsofia.limbek@gmail.com>
2021-05-25 23:18:19 +0200

    implemented selection sort, comb sort, enhanced cocktail sort

--- sources/algorithms/sort.py
+++ sources/algorithms/sort.py
@@ -1,23 +1,84 @@
+import math
+
+
 def insertion_sort(array):
-    result = []
+    out = []
     for element in array:
-        if len(result) == 0:
-            result.append(element)
+        if len(out) == 0:
+            out.append(element)
         else:
             i = 0
-            while i <= len(result) - 1 and element > result[i]:
+            while i <= len(out) - 1 and element > out[i]:
                 i += 1
-            result.insert(i, element)
-    return result
+            out.insert(i, element)
+    return out
 
 
 def bubble_sort(array):
-    result = array[:]
+    out = array[:]
     n = len(array)
     for i in range(n):
         for j in range(n - i - 1):
-            if result[j] > result[j + 1]:
-                temp = result[j]
-                result[j] = result[j + 1]
-                result[j + 1] = temp
-    return result
+            if out[j] > out[j + 1]:
+                temp = out[j]
+                out[j] = out[j + 1]
+                out[j + 1] = temp
+    return out
+
+
+def selection_sort(array):
+    out = array[:]
+    for i in range(len(out)):
+        index_of_minimal_element = i
+        for j in range(i + 1, len(out)):
+            if out[j] < out[index_of_minimal_element]:
+                index_of_minimal_element = j
+        temp = out[index_of_minimal_element]
+        out[index_of_minimal_element] = out[i]
+        out[i] = temp
+    return out
+
+
+def comb_sort(array):
+    shrink = 1.3
+    out = array[:]
+    gap = len(out)
+
+    while gap > 1:
+        gap = math.floor(gap / shrink)
+        if gap <= 1:
+            gap = 1
+        for i in range(len(out) - gap):
+            if out[i] > out[i + gap]:
+                out[i], out[i + gap] = out[i + gap], out[i]
+    return out
+
+
+def enhanced_cocktail_sort(array):
+    out = array[:]
+    low = 0
+    high = len(out) - 1
+
+    while low < high:
+        swapped = False
+        for i in range(low, high):
+            if out[i] > out[i + 1]:
+                out[i], out[i + 1] = out[i + 1], out[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+        high -= 1
+
+        for i in range(high, low, -1):
+            if out[i] < out[i - 1]:
+                out[i - 1], out[i] = out[i], out[i - 1]
+                swapped = True
+
+        if not swapped:
+            break
+        low += 1
+
+    return out
--- test/test_sort.py
+++ test/test_sort.py
@@ -26,3 +26,39 @@ def test_insertion_sort(array, expected):
 def test_bubble_sort(array, expected):
     actual = sort.bubble_sort(array)
     testing_util.assert_list(actual, expected)
+
+
+@pytest.mark.parametrize("array,expected",
+                         [
+                             ([0, 3, 2, 7, 5, 1, 6], [0, 1, 2, 3, 5, 6, 7]),
+                             ([2, -5, 7, 4, 3, 1], [-5, 1, 2, 3, 4, 7]),
+                             ([-3, 5, 0, 1, 8, 0], [-3, 0, 0, 1, 5, 8]),
+                             ([99, -43, 76, -50, 0], [-50, -43, 0, 76, 99])
+                         ])
+def test_selection_sort(array, expected):
+    actual = sort.selection_sort(array)
+    testing_util.assert_list(actual, expected)
+
+
+@pytest.mark.parametrize("array,expected",
+                         [
+                             ([0, 3, 2, 7, 5, 1, 6], [0, 1, 2, 3, 5, 6, 7]),
+                             ([2, -5, 7, 4, 3, 1], [-5, 1, 2, 3, 4, 7]),
+                             ([-3, 5, 0, 1, 8, 0], [-3, 0, 0, 1, 5, 8]),
+                             ([99, -43, 76, -50, 0], [-50, -43, 0, 76, 99])
+                         ])
+def test_comb_sort(array, expected):
+    actual = sort.comb_sort(array)
+    testing_util.assert_list(actual, expected)
+
+
+@pytest.mark.parametrize("array,expected",
+                         [
+                             ([0, 3, 2, 7, 5, 1, 6], [0, 1, 2, 3, 5, 6, 7]),
+                             ([2, -5, 7, 4, 3, 1], [-5, 1, 2, 3, 4, 7]),
+                             ([-3, 5, 0, 1, 8, 0], [-3, 0, 0, 1, 5, 8]),
+                             ([99, -43, 76, -50, 0], [-50, -43, 0, 76, 99])
+                         ])
+def test_enhanced_cocktail_sort(array, expected):
+    actual = sort.enhanced_cocktail_sort(array)
+    testing_util.assert_list(actual, expected)
